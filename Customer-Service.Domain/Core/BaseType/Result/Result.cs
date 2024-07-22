6cc0c7dd4f6d863d6a03b3bb8d316e99c5938657
Ebrahem-Outlook <EbrahemElbetar1@outlook.com>
2024-07-22 22:34:58 +0300

    V12

--- Customer-Service.Domain/Core/BaseType/Result/Result.cs
+++ Customer-Service.Domain/Core/BaseType/Result/Result.cs
@@ -1,6 +1,23 @@
-﻿namespace Customer_Service.Domain.Core.BaseType.Result;
+﻿using System.Data.SqlTypes;
+
+namespace Customer_Service.Domain.Core.BaseType.Result;
 
 public class Result
 {
+    private Result(bool isSuccess, Error error)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
 
+    public bool IsSuccess { get; }
+
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; }
+
+
+    public static Result Success() => new Result(true, Error.None);
+
+    public static Result Failure(Error error) => new Result(false, error); 
 }
