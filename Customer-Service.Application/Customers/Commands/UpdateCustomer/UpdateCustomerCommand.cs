092dee9c84974c8301de49d7b2b315875cd7d5b5
Ebrahem-Outlook <EbrahemElbetar1@outlook.com>
2024-07-30 13:37:53 +0300

    V_17

---
+++ Customer-Service.Application/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,7 @@
+﻿using Customer_Service.Application.Core.Messaging;
+
+namespace Customer_Service.Application.Customers.Commands.UpdateCustomer;
+
+public sealed record UpdateCustomerCommand(
+    Guid CustomerId, 
+    ) : ICommand
